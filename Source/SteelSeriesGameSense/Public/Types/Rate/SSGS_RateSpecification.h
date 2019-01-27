6d987b3cf8ccf17a51721e5398dca77a1a751efd
sharkgoesmad <sharkgoesmad@gmail.com>
2019-01-27 10:26:33 -0600

    Document rate specification functions

--- Source/SteelSeriesGameSense/Public/Types/Rate/SSGS_RateSpecification.h
+++ Source/SteelSeriesGameSense/Public/Types/Rate/SSGS_RateSpecification.h
@@ -17,9 +17,22 @@ class STEELSERIESGAMESENSE_API USSGS_RateSpecification : public UObject, public 
 
 public:
 
+    /**
+    * Properly constructs USSGS_RateSpecification with static variant.
+    *
+    * @param    frequency Specifies the frequency component of the rate object in Hz.
+    * @param    repeat_limit Repeat count for the rate object.
+    * @return   USSGS_ColorEffectSpecificationRanges object.
+    */
     UFUNCTION( BlueprintCallable, BlueprintPure, Category = "GameSense|RateSpecification" )
     static USSGS_RateSpecification* MakeStaticRate( int32 frequency, int32 repeat_limit );
 
+    /**
+    * Properly constructs USSGS_RateSpecification with ranges variant.
+    *
+    * @param    v Array of ranges.
+    * @return   USSGS_ColorEffectSpecificationRanges object.
+    */
     UFUNCTION( BlueprintCallable, BlueprintPure, Category = "GameSense|RateSpecification" )
     static USSGS_RateSpecification* MakeRangedRate( UPARAM( DisplayName = "Frequency - RepeatLimit Pairs" ) const TArray< FSSGS_FrequencyRepeatLimitPair >& v );
 
