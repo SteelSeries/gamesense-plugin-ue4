5e99060c2e820f561d85cf383fb52d8195cc33cf
sharkgoesmad <sharkgoesmad@gmail.com>
2019-01-26 21:07:53 -0600

    Document tactile pattern construction functions

--- Source/SteelSeriesGameSense/Public/Types/Haptics/SSGS_TactilePatternSpecificationRanges.h
+++ Source/SteelSeriesGameSense/Public/Types/Haptics/SSGS_TactilePatternSpecificationRanges.h
@@ -13,6 +13,12 @@ class STEELSERIESGAMESENSE_API USSGS_TactilePatternSpecificationRanges : public 
 
 public:
 
+    /**
+    * Properly constructs USSGS_TactilePatternSpecificationRanges object.
+    *
+    * @param    ranges Array of ranges.
+    * @return   USSGS_HandlerCollection object.
+    */
     UFUNCTION( BlueprintCallable, BlueprintPure, Category = "GameSense|TactilePatternSpecification" )
     static USSGS_TactilePatternSpecificationRanges* MakeRangedTactilePattern( const TArray< FSSGS_TactilePatternRange >& ranges );
 
--- Source/SteelSeriesGameSense/Public/Types/Haptics/SSGS_TactilePatternSpecificationStatic.h
+++ Source/SteelSeriesGameSense/Public/Types/Haptics/SSGS_TactilePatternSpecificationStatic.h
@@ -13,12 +13,32 @@ class STEELSERIESGAMESENSE_API USSGS_TactilePatternSpecificationStatic : public 
 
 public:
 
+    /**
+    * Properly constructs USSGS_TactilePatternSpecificationStatic object.
+    *
+    * @param    pattern Array of variant (predefined/custom) pattern objects.
+    * @return   USSGS_TactilePatternSpecificationStatic object.
+    */
     UFUNCTION( BlueprintCallable, BlueprintPure, Category = "GameSense|TactilePatternSpecification" )
     static USSGS_TactilePatternSpecificationStatic* MakeStaticTactilePattern( const TArray< FSSGS_TactilePatternStatic >& pattern );
 
+    /**
+    * Construct a predefined static pattern variant.
+    *
+    * @param    type Predefined tactile pattern. Use USSGS_PredefinedTactilePatternFactory to obtain correct value.
+    * @param    delay_ms Delay amount in milliseconds.
+    * @return   FSSGS_TactilePatternStatic object.
+    */
     UFUNCTION( BlueprintCallable, BlueprintPure, meta = ( NativeMakeFunc, Category = "GameSense|TactilePatternSpecification" ) )
     static FSSGS_TactilePatternStatic MakeStaticWithPredefinedEffect( const FSSGS_PredefinedTactilePattern& type, int32 delay_ms = 0 );
 
+    /**
+    * Construct a custom static pattern variant.
+    *
+    * @param    length_ms Duration of the effect in milliseconds.
+    * @param    delay_ms Delay amount in milliseconds.
+    * @return   FSSGS_TactilePatternStatic object.
+    */
     UFUNCTION( BlueprintCallable, BlueprintPure, meta = ( NativeMakeFunc, Category = "GameSense|TactilePatternSpecification" ) )
     static FSSGS_TactilePatternStatic MakeStaticWithCustomEffect( int32 length_ms, int32 delay_ms = 0 );
 
